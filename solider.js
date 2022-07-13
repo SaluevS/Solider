289d8f1a86f1d1079a24b051bcd3427ba71ad93a
Saluev <ibragim.saluev@mail.ru>
2022-07-13 12:54:31 +0300

    Солдат

---
+++ solider.js
@@ -0,0 +1,16 @@
+//1
+const solider = {
+    name: 'Bob',
+    HP: 10,
+    Gun: {
+       name: 'Глок',
+       patron: 30,
+    },
+    supplies: 3,
+
+    fire: function () {
+        
+    },
+    recharge: function () {},
+    hurt: function () {},
+}
